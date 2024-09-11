dd4e5e2c034d585ea6b68df7766a18a906b30d79
Marc Justyn <mjsvillanueva2440@gmail.com>
2024-09-11 19:03:30 +0800

    ENV File Fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env': {
-		  VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
-		  VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY
-		}
-	}
 });
